3a604496c7c1675245539660ee90818e04703e97
Adam Struthers <adam@serouslabs.com>
2014-11-18 16:18:58 -0500

    a

--- js/server.js
+++ js/server.js
@@ -24,3 +24,4 @@ app.post('/post', function( req, res) {
 app.set('port', process.env.PORT || 8000);
 http.createServer(app).listen(app.get('port'), function(){
     console.log("Express server listening on port " + app.get('port'));
+});
